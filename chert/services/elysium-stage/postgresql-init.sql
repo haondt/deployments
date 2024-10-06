855c06b56d1d35fe5349ce42228a64dae2c59cf8
Noah Burghardt <burghardtnoah@gmail.com>
2024-10-05 20:20:53 -0600

    I do not think this will work

---
+++ chert/services/elysium-stage/postgresql-init.sql
@@ -0,0 +1,20 @@
+\connect elysium;
+
+CREATE TABLE IF NOT EXISTS "elysium" (
+    PrimaryKey TEXT PRIMARY KEY,
+    KeyString TEXT NOT NULL,
+    Value JSONB NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "foreignKeys" (
+    id SERIAL PRIMARY KEY,
+    ForeignKey TEXT,
+    KeyString TEXT NOT NULL,
+    PrimaryKey TEXT REFERENCES "elysium"(PrimaryKey) ON DELETE CASCADE
+);
+
+CREATE INDEX IF NOT EXISTS idx_foreign_key
+ON "foreignKeys"(ForeignKey);
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO elysium;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO elysium;
