1485df96962fccc07b88b938714a727ac356ad8e
Noah Burghardt <burghardtnoah@gmail.com>
2025-03-01 07:23:43 -0700

    dump more info

--- gabbro/services/talos/app.py
+++ gabbro/services/talos/app.py
@@ -1,5 +1,6 @@
 import logging
 from flask import Flask, request
+import json
 
 # Set up logging
 logging.basicConfig(
@@ -16,8 +17,18 @@ def pipeline_webhook():
     logger.info("Received request:")
     logger.info(f"URL: {request.url}")
     logger.info(f"Requester IP: {request.remote_addr}")
-    logger.info(f"Headers: {dict(request.headers)}")
-    logger.info(f"Body: {request.get_json()}")
+    headers_string = ""
+    try:
+        headers_string = json.dumps(dict(request.headers), indent=4)
+    except:
+        headers_string = str(dict(request.headers))
+    logger.info(f"Headers: {headers_string}")
+    body_string = ""
+    try:
+        body_string = json.dumps(request.get_json(), indent=4)
+    except:
+        body_string = request.get_data().decode('utf-8')
+    logger.info(f"Body: {body_string}")
     return "OK", 200
 
 if __name__ == "__main__":
